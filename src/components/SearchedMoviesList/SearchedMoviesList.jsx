a916b9d609892cc13c2f15cff6cd7c076011fa47
Marta Tabor <leeloothefifth@googlemail.com>
2024-05-11 22:36:42 +0100

    search funtion update

---
+++ src/components/SearchedMoviesList/SearchedMoviesList.jsx
@@ -0,0 +1,44 @@
+import { useSearchParams, Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { fetchMoviesByQuery } from '../../utilities/MoviesByQueryFetch';
+import css from './SearchedMoviesList.module.css';
+
+export const SearchedMoviesList = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get('query');
+  const location = useLocation();
+
+  useEffect(() => {
+    if (query) {
+      fetchMovies(query);
+    } else {
+      setMovies([]);
+    }
+  }, [query]);
+
+  const fetchMovies = async query => {
+    try {
+      const movies = await fetchMoviesByQuery(query);
+      setMovies(movies);
+      console.log('Movies:', movies);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    }
+  };
+
+  return (
+    <div>
+      {query && <h2>Search Results:</h2>}
+      <ul className={css.list}>
+        {movies.map(movie => (
+          <li className={css.item} key={movie.id}>
+            <Link className={css.link} to={movie.id} state={{ from: location }}>
+              {movie.title || movie.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,40 +1,11 @@
 import { SearchBox } from 'components/SearchBox/SearchBox';
-import { useSearchParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { fetchMoviesByQuery } from '../../utilities/MoviesByQueryFetch';
+import { SearchedMoviesList } from 'components/SearchedMoviesList/SearchedMoviesList';
 
-const Movies = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchParams] = useSearchParams();
-  const query = searchParams.get('query');
-
-  useEffect(() => {
-    if (query) {
-      fetchMovies(query);
-    } else {
-      setMovies([]);
-    }
-  }, [query]);
-
-  const fetchMovies = async query => {
-    try {
-      const movies = await fetchMoviesByQuery(query);
-      setMovies(movies);
-      console.log('Movies:', movies);
-    } catch (error) {
-      console.error('Error fetching movies:', error);
-    }
-  };
-
+const Movies = ({ movies }) => {
   return (
     <main>
       <SearchBox />
-      {query && <h2>Search Results:</h2>}
-      <ul>
-        {movies.map(movie => (
-          <li key={movie.id}>{movie.title || movie.name}</li>
-        ))}
-      </ul>
+      <SearchedMoviesList movies={movies} />
     </main>
   );
 };
