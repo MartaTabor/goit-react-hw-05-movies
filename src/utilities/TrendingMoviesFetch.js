be8470b75b91b3d9a4580c4215a439b8ffcdf3d7
Marta Tabor <leeloothefifth@googlemail.com>
2024-05-12 21:55:17 +0100

    update to trending movies url

--- src/utilities/TrendingMoviesFetch.js
+++ src/utilities/TrendingMoviesFetch.js
@@ -4,7 +4,7 @@ import { API_KEY } from './Api';
 export const fetchTrendingMovies = async () => {
   try {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${API_KEY}`
+      `https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=${API_KEY}`
     );
     return response;
   } catch (error) {
